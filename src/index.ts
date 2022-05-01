b94f1f8af0a3e5939aee4622c82890cdcda3f71c
Aneks1 <proames75yt@gmail.com>
2022-05-01 12:09:56 -0500

    uwu

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,4 @@ import getUserFromID from "./getData/getGJUserInfo20"
 import getCommentsFromPlayerID from "./getData/getGJAccountComments"
 import Client from "./util/client"
 
-export default { getUserFromID, Client }
-
-async function name() {
-
-    const me = new Client()
-    
-    const a = await me.login({ username: "Tofixts", password: "sussus" })
-
-    const comments = await getCommentsFromPlayerID({ id: me.profile.info.playerID })
-}
-
-name()
+export default { getUserFromID, Client, getCommentsFromPlayerID }
